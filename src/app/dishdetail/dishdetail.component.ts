e729f5c1128650a986f9cde70a7f750441bbdd3f
nolan singroy <nolansingroy@gmail.com>
2019-11-04 12:44:11 -0600

    Single Page Application Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input  } from '@angular/core';
 import { Dish } from '../shared/dish';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { DishService } from '../services/dish.service';
+
 
 // Task 2.1 Add DISh data Object to file
 const DISH = {
@@ -59,9 +63,19 @@ export class DishdetailComponent implements OnInit {
   //dish = DISH;
   records = DISH.comments;
 
-  constructor() { }
+  constructor(private dishservice: DishService,
+                private route: ActivatedRoute,
+                private location: Location) { }
 
   ngOnInit() {
-  }
+
+    const id = this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
+}
+
+
+    goBack(): void {
+        this.location.back()
+    }
 
 }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,27 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes';
-import { DishService } from '../services/dish.service';
 
- @Component({
+@Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-
 export class MenuComponent implements OnInit {
 
-  dishes: Dish[] = DISHES;
+  constructor() { }
 
-  selectedDish: Dish;
-
-  onSelect(dish: Dish) {
-    this.selectedDish = dish;
-  }
-  constructor(private dishService: DishService) {  }
-  
   ngOnInit() {
-	this.dishes = this.dishService.getDishes();
   }
 
 }
