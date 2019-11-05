e8d7b557ba7dfaa804b62413730685f6638649cd
nolan singroy <nolansingroy@gmail.com>
2019-11-05 17:00:29 -0600

    docs: Converting About component to SPA

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
+
+//todo: fetch the details of the leaders and make them aviable to the template,
+
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
