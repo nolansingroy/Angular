6a6aa8af27c58324987b2df2350ec4f265f95582
nolan singroy <nolansingroy@gmail.com>
2019-11-05 16:40:39 -0600

    Fix: updated leader service I added a leader.ts(data model class) , leaders.ts(json object), and service/leader.service.ts(actual implementation logic) files to handle the json throughout the application updating the app.module.ts file providers array! Using this leader service any app component can now have access to this leader object

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,16 +1,28 @@
-
 import { Injectable } from '@angular/core';
-import { LEADERS } from '../shared/leader';
-//Todo: Add the details of the corporate leaders here.
-
+import { Leader } from '../shared/leader';
+import { LEADERS } from '../shared/leaders';
 
 @Injectable({
   providedIn: 'root'
 })
 
-
-
 export class LeaderService {
 
   constructor() { }
+
+/*
+ * The purpose of the servive is to provide the details 
+ * of the corporate leaders given in leaders.txt above
+ **/
+
+
+/*
+* Simple function to return the LEADERS constant
+* that holds the json file
+*/
+getLeader(): Leader[] {
+  return LEADERS;
+}
+
+
 }
