bf9316ea1fd569b29fa91d341b0a4203800117e1
nolan singroy <nolansingroy@gmail.com>
2019-11-10 18:07:21 -0600

    feat: updated About Compontent to render mat-card-avatar img also updated home component to retrieve leaderService data and render it!

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,8 @@ import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { Promotion } from '../shared/promotion';
 import { PromotionService } from '../services/promotion.service';
+import { LeaderService } from '../services/leader.service';
+import { Leader } from '../shared/leader';
 
 @Component({
   selector: 'app-home',
@@ -13,17 +15,18 @@ import { PromotionService } from '../services/promotion.service';
 export class HomeComponent implements OnInit {
 
  //update the component to fetch and provide the feature dish and promtion view
-
   dish: Dish;
   promotion: Promotion;
+  leaders: Leader[];
 
    constructor(private dishService: DishService,
-                private promotionService: PromotionService) { }
-
+                private promotionService: PromotionService,
+                private leaderService: LeaderService) { }
 
   ngOnInit() {
     this.dish = this.dishService.getFeaturedDish();
     this.promotion = this.promotionService.getFeaturedPromotion();
+    this.leaders = this.leaderService.getLeaders();
   }
 
 }
