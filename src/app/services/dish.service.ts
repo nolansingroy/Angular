875d593305bae74344abf373046c6269d2d11965
nolan singroy <nolansingroy@gmail.com>
2019-11-20 19:34:44 -0600

    RxJS Part 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -30,4 +30,10 @@ getDish(id: string): Observable<Dish> {
 	return of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000));
 }
 
+
+getDishIds(): Observable<string[] | any>{
+	return of(DISHES.map( dish=> dish.id ));
+}
+
+
 }
